1c8503bd9bca1be987a1d23fe6385167c2116d95
kyrelos <kalosobat@gmail.com>
2017-03-14 22:05:05 +0300

    register models in admin

--- app_dir/wallet_transactions/admin.py
+++ app_dir/wallet_transactions/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Transaction
+from .models import Transaction, BulkTransaction, BulkTransactionLookup
 
 admin.site.register(Transaction)
+admin.site.register(BulkTransaction)
+admin.site.register(BulkTransactionLookup)
