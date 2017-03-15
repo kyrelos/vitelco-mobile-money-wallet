4fd8d7b369ba0a74b800efb7f61c6dbec17f5c6a
kyrelos <kalosobat@gmail.com>
2017-03-16 01:12:06 +0300

    update url naming

--- urls.py
+++ urls.py
@@ -5,7 +5,7 @@ from app_dir.notification_management import urls as notification_urls
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^notification/', include(notification_urls)),
+    url(r'^api/v1/notification/', include(notification_urls)),
 
 
 
