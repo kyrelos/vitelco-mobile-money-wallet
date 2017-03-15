0deb9c20aaf8ffd43ff5c0b8b8167082e2bac16c
kyrelos <kalosobat@gmail.com>
2017-03-16 01:00:56 +0300

    add documentation for NotificationSerializer

--- app_dir/notification_management/serializers.py
+++ app_dir/notification_management/serializers.py
@@ -5,6 +5,13 @@ from .models import Notification
 
 
 class NotificationSerializer(serializers.ModelSerializer):
+    """
+    This is a serializer for the ListCcreateNotification view. Its is
+    configured to use customers wallet_id instead of primary keys for
+    lookup and creating the customer wallet object.
+    The main reason for this is that wallet_id will be more consistent
+    compared to primary key.
+    """
     customer = serializers.SlugRelatedField(
             many=False,
             read_only=False,
