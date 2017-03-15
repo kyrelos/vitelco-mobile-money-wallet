9e88df04b8b1904d50b68f09eadee9165e66a523
Lulu Karega <aminakarega@gmail.com>
2017-03-15 12:18:58 +0300

    Add status to customer

--- app_dir/customer_wallet_management/models.py
+++ app_dir/customer_wallet_management/models.py
@@ -14,10 +14,21 @@ class CustomerWallet(models.Model):
         ("normal", "normal"),
         ("merchant", "merchant")
     )
+
+    STATUS_TYPES = (
+        ("active","active"),
+        ("dormant", "dormant"),
+        ("inactive", "inactive")
+    )
+
     wallet_id = models.UUIDField(unique=True)
     msisdn = models.CharField(max_length=20, unique=True)
     token = models.CharField(max_length=256, unique=True)
     name = models.CharField(max_length=120)
+    status = models.CharField(max_length=20,
+                              choices=STATUS_TYPES,
+                              default="active"
+                              )
     type = models.CharField(max_length=20,
                             choices=CUSTOMER_TYPES,
                             default="normal"
