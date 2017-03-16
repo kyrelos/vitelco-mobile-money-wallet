bd71c576596f5cd02bc3c51acd0168ff246e8306
kyrelos <kalosobat@gmail.com>
2017-03-16 22:35:48 +0300

    add get accountname view

--- app_dir/customer_wallet_management/views.py
+++ app_dir/customer_wallet_management/views.py
@@ -1,14 +1,17 @@
+from datetime import datetime
+
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import Http404
-from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.views import APIView
+
 from .models import CustomerWallet
 from .serializers import CustomerWalletSerializer, \
     CustomerWalletStatusSerializer
 
 
 class CustomerWalletViewSet(APIView):
-
     def get_object(self, pk):
         try:
             return CustomerWallet.objects.get(pk=pk)
@@ -42,7 +45,6 @@ class CustomerWalletViewSet(APIView):
 
 
 class getAccountStatus(APIView):
-
     def get(self, request, msisdn):
         account = CustomerWallet.objects.get(msisdn=msisdn)
         if account is not None:
@@ -50,3 +52,122 @@ class getAccountStatus(APIView):
             return Response(serializer.data)
         else:
             return Response(status.HTTP_404_NOT_FOUND)
+
+
+class GetAccountName(APIView):
+    """
+    This API retrieves the customers name details given a msisdn
+    HTTP Method: GET
+    URI: /api/v1/accounts/msisdn/{msisdn}/accountname/
+
+    Required HTTP Headers:
+    DATE: todays date
+    AUTHORIZATION: api-key
+    CONTENT-TYPE: application/json
+
+    Success response:
+    HTTP status code: 200
+    {
+        "name": {
+            "title": "",
+            "firstName": first_name,
+            "middleName": "",
+            "lastName": "",
+            "fullName": "",
+            "nativeName": ""
+        },
+        "status": "",
+        "lei": ""
+    }
+
+    Error response: [404, 400, account in inactive state,
+                    DATE header not supplied]
+
+    {
+        "errorCategory": "businessRule",
+        "errorCode": "genericError",
+        "errorDescription": "string",
+        "errorDateTime": "string",
+        "errorParameters": [
+            {
+                "key": key,
+                "value": value
+            }
+        ]
+    }
+
+
+    """
+    def get(self, request, msisdn):
+        date = request.META.get("HTTP_DATE")
+        if not date:
+            return self.send_error_response(
+                    message="DATE Header not supplied",
+                    key="DATE",
+                    status=status.HTTP_400_BAD_REQUEST
+                    )
+
+        try:
+            account = CustomerWallet.objects.get(msisdn=msisdn)
+            first_name = account.name
+            account_status = account.status
+            if account_status == CustomerWallet.active:
+                payload = {
+                    "name": {
+                        "title": "",
+                        "firstName": first_name,
+                        "middleName": "",
+                        "lastName": "",
+                        "fullName": "",
+                        "nativeName": ""
+                    },
+                    "status": "",
+                    "lei": ""
+                }
+                response = Response(data=payload,
+                                    content_type="application/json",
+                                    status=status.HTTP_200_OK
+                                    )
+                return response
+            else:
+                return self.send_error_response(
+                            message="Requested resource not active",
+                            key="msisdn",
+                            value=msisdn,
+                            status=status.HTTP_404_NOT_FOUND
+                )
+
+        except ObjectDoesNotExist:
+
+            return self.send_error_response(
+                    message="Requested resource not available",
+                    key="msisdn",
+                    value=msisdn,
+                    status=status.HTTP_404_NOT_FOUND
+            )
+
+    @staticmethod
+    def send_error_response(message="404",
+                            key="msisdn",
+                            value=None,
+                            status=None,
+                            ):
+        date_time = datetime.now().isoformat()
+        error_payload = {
+            "errorCategory": "businessRule",
+            "errorCode": "genericError",
+            "errorDescription": message,
+            "errorDateTime": date_time,
+            "errorParameters": [
+                {
+                    "key": key,
+                    "value": value
+                }
+            ]
+        }
+
+        response = Response(data=error_payload,
+                            content_type="application/json",
+                            status=status
+                            )
+        return response
