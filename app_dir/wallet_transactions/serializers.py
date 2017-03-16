51bf4d5778ea0580bfc09679b6430fff5d6a92ef
urandu <bildad.urandu@jumo.world>
2017-03-16 14:31:29 +0300

    added transaction list_create

---
+++ app_dir/wallet_transactions/serializers.py
@@ -0,0 +1,45 @@
+from rest_framework import serializers
+
+from app_dir.customer_wallet_management.models import CustomerWallet
+from app_dir.wallet_transactions.models import Transaction
+
+
+class TransactionSerializer(serializers.ModelSerializer):
+    """
+    This is a serializer for the CreateNotification view.
+    """
+
+    source = serializers.SlugRelatedField(
+        many=False,
+        read_only=False,
+        slug_field='wallet_id',
+        queryset=CustomerWallet.objects.all()
+    )
+
+    destination = serializers.SlugRelatedField(
+        many=False,
+        read_only=False,
+        slug_field='wallet_id',
+        queryset=CustomerWallet.objects.all()
+    )
+
+    class Meta:
+        model = Transaction
+        fields = ["trid", "source", "destination", "amount", "type"]
+
+
+class BulkTransactionSerializer(serializers.ModelSerializer):
+    """
+    This is a serializer for the CreateNotification view.
+    """
+
+    merchant = serializers.SlugRelatedField(
+        many=False,
+        read_only=False,
+        slug_field='wallet_id',
+        queryset=CustomerWallet.objects.all()
+    )
+
+    class Meta:
+        model = Transaction
+        fields = ["bulk_trid", "merchant"]
