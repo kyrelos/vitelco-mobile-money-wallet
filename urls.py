bf9d2d393b740eb34ddaf195133a0b491f10ff5c
Anne Chepkeitany <anne.chepkeitany@jumo.world>
2017-03-16 10:40:45 +0300

    Use api/v1 for the urls

--- urls.py
+++ urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from app_dir.notification_management import urls as notification_urls
+from app_dir.customer_wallet_management import urls as customer_wallet_urls
 
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/v1/notification/', include(notification_urls)),
-    url(r'^', include('app_dir.customer_wallet_management.urls')),
+    url(r'^api/v1', include(customer_wallet_urls)),
 ]
