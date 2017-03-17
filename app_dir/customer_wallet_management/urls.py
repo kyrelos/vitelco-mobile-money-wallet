b3eebdfa0073b63ee56260fab79c177dad40fd8f
Lulu Karega <aminakarega@gmail.com>
2017-03-17 16:13:21 +0300

    Using ISO standard for MSISDN

--- app_dir/customer_wallet_management/urls.py
+++ app_dir/customer_wallet_management/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.CustomerWalletViewSet.as_view()),
     url(r'^(?P<pk>[0-9]+)/$', views.CustomerWalletViewSet.as_view()),
-    url(r'^msisdn/(?P<msisdn>[0-9]+)/status/$',
+    url(r'^msisdn/(?P<msisdn>[+][0-9]+)/status/$',
         views.GetAccountStatusByMSISDN.as_view()),
     url(r'^(?P<wallet_id>[0-9a-zA-z\-]+)/status/$',
         views.GetAccountStatusByUUID.as_view()),
