045a8bb5efaecf379c81071d9b166e466bab44d3
urandu <bildad.urandu@jumo.world>
2017-03-18 01:30:44 +0300

    transaction/{transactionReference endpoint added}

--- app_dir/wallet_transactions/views.py
+++ app_dir/wallet_transactions/views.py
@@ -1,37 +1,178 @@
+from datetime import datetime
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import generics
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .models import Transaction
 from .serializers import TransactionSerializer
 from rest_framework.permissions import IsAdminUser
+from structlog import get_logger
+
+logger = get_logger("transactions")
 
 
-class TransactionCreateList(generics.ListCreateAPIView):
-    """
-    This Api creates a transaction object for a single transaction
-    HTTP method: POST
-    URI: /transactions/list_create
-
-    Example payload:
-    {
-        "trid": "24f6654c-9e76-419d-a55a-cb6ab520af4c",
-        "source": "d4664ed6-0a39-11e7-b188-c7bc39d6527f",
-        "destination": "153b8e3e-0a3b-11e7-8880-3f4930b3363a",
-        "type": "deposit",
-        "amount": "100"
-    }
-
-
-    trid: :type(string) A UUID string as transaction id
-    source: :type(string) A UUID string representing the customers wallet_id
-    destination: :type(string) A UUID string representing the customers
-     wallet_id
-    type: :type(string) Type of the message
-     ["reversal", "payment", "deposit", "withdrawal", "p2p"]
-    amount: :type(integer) an integer represent the amount of money
-
-    Expected Response:
-    Expected Response:
-    201: created
-    """
+class TransactionList(generics.ListAPIView):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
     permission_classes = (IsAdminUser,)
+
+
+class TransactionRetrieve(generics.RetrieveAPIView):
+    serializer_class = TransactionSerializer
+    permission_classes = (IsAdminUser,)
+
+    def get_queryset(self):
+        """
+        This view should return a list of all the purchases for
+        the user as determined by the username portion of the URL.
+        """
+        transaction_id = self.kwargs['transactionReference']
+        return Transaction.objects.filter(trid=transaction_id)
+
+
+class GetTransaction(APIView):
+    """
+    This API retrieves a transaction given the transaction reference
+    HTTP Method: GET
+    URI: /api/v1/transactions/{transactionReference}
+    Required HTTP Headers:
+    DATE: todays date
+    AUTHORIZATION: api-key
+    CONTENT-TYPE: application/json
+    Success response:
+    HTTP status code: 200
+    { //this is to be changed once correct
+         "transaction": {
+                    "reference": transaction_reference,
+                    "type": transaction_type,
+                    "amount": transaction_amount,
+                    "date": transaction_date,
+                    "status": transaction_state
+                }
+    }
+    Error response: [404, 400, DATE header not supplied]
+    {
+        "errorCategory": "businessRule",
+        "errorCode": "genericError",
+        "errorDescription": "string",
+        "errorDateTime": "string",
+        "errorParameters": [
+            {
+                "key": key,
+                "value": value
+            }
+        ]
+    }
+    """
+
+    def get(self, request, transaction_reference):
+        date = request.META.get("HTTP_DATE")
+        if not date:
+            logger.info("get_transaction_400",
+                        message="DATE Header not supplied",
+                        status=status.HTTP_400_BAD_REQUEST,
+                        transaction_reference=transaction_reference,
+                        key="DATE"
+                        )
+            return self.send_error_response(
+                message="DATE Header not supplied",
+                key="DATE",
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        try:
+            transaction = Transaction.objects.get(trid=transaction_reference)
+
+            if not transaction:
+                logger.info("get_transaction_404",
+                            status=status.HTTP_404_NOT_FOUND,
+                            trid=transaction_reference,
+                            key="trid"
+                            )
+
+                return self.send_error_response(
+                    message="Requested resource not available",
+                    key="trid",
+                    value=transaction_reference,
+                    status=status.HTTP_404_NOT_FOUND
+                )
+
+            transaction_state = transaction.state
+            transaction_type = transaction.type
+            transaction_amount = transaction.amount
+            transaction_date = transaction.created_at
+
+            payload = {
+                "transaction": {
+                    "reference": transaction_reference,
+                    "type": transaction_type,
+                    "amount": transaction_amount,
+                    "date": transaction_date,
+                    "status": transaction_state
+                }
+            }
+            response = Response(data=payload,
+                                content_type="application/json",
+                                status=status.HTTP_200_OK
+                                )
+            logger.info("get_transaction_200",
+                        status=status.HTTP_200_OK,
+                        trid=transaction_reference
+                        )
+            return response
+
+        except ObjectDoesNotExist:
+            logger.info("get_transaction_404",
+                        status=status.HTTP_404_NOT_FOUND,
+                        trid=transaction_reference,
+                        key="trid"
+                        )
+
+            return self.send_error_response(
+                message="Requested resource not available",
+                key="trid",
+                value=transaction_reference,
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        except ValueError:
+            logger.info("get_transaction_malformed_uuid",
+                        status=status.HTTP_404_NOT_FOUND,
+                        trid=transaction_reference,
+                        key="trid"
+                        )
+
+            return self.send_error_response(
+                message="Malformed UUID",
+                key="trid",
+                value=transaction_reference,
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+    @staticmethod
+    def send_error_response(message="404",
+                            key="trid",
+                            value=None,
+                            status=None,
+                            ):
+        date_time = datetime.now().isoformat()
+        error_payload = {
+            "errorCategory": "businessRule",
+            "errorCode": "genericError",
+            "errorDescription": message,
+            "errorDateTime": date_time,
+            "errorParameters": [
+                {
+                    "key": key,
+                    "value": value
+                }
+            ]
+        }
+
+        response = Response(data=error_payload,
+                            content_type="application/json",
+                            status=status
+                            )
+        return response
