edb1878bfbca7c8ad2b2888da65291a8bbd906c4
kyrelos <kalosobat@gmail.com>
2017-03-18 17:26:19 +0300

    add root api view for browsable API

--- app_dir/wallet_api/views.py
+++ app_dir/wallet_api/views.py
@@ -1,9 +1,81 @@
 from django.http import Http404
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from django.core.urlresolvers import reverse as django_reverse
 from rest_framework.views import APIView
 from app_dir.wallet_transactions.models import BatchTransaction
 from app_dir.wallet_transactions.serializers import BatchTransactionSerializer
+
+
+class APIRootView(APIView):
+    def get(self, request):
+        api_registry = {
+            "Transactions": {
+                "Get transaction by transaction_reference": reverse(
+                    "get_transaction_by_transaction_reference",
+                    request=request,
+                    current_app="wallet_transactions",
+                    kwargs={
+                        "transaction_reference":
+                            "753bcd19-7230-40ba-a975-09ac94ace0d2"
+                    }
+
+
+                )
+
+            },
+            "BatchTransactions": {
+                "Create batch transactions": reverse(
+                    "batchtransactions", request=request
+                )
+            },
+            "Account": {
+                "Get Account status by msisdn": reverse(
+                    "account:msisdn",
+                    request=request,
+                    current_app="customer_wallet_management",
+                    kwargs={"msisdn": "+254711111111"}
+                ),
+                "Get Account status by accountId": reverse(
+                    "account:get_account_status_by_account_id",
+                    request=request,
+                    current_app="customer_wallet_management",
+                    kwargs={
+                        "wallet_id": "753bcd19-7230-40ba-a975-09ac94ace0d2"
+                    }
+                ),
+                "Get Account name by msisdn": reverse(
+                    "account:get_account_name_by_msisdn",
+                    request=request,
+                    current_app="customer_wallet_management",
+                    kwargs={"msisdn": "254711111111"}
+                ),
+                "Get Account balance by msisdn": reverse(
+                    "account:get_account_balance_by_msisdn",
+                    request=request,
+                    current_app="customer_wallet_management",
+                    kwargs={"msisdn": "254711111111"}
+                ),
+                "Get Account balance by accountId": reverse(
+                    "account:get_account_balance_by_account_id",
+                    request=request,
+                    current_app="customer_wallet_management",
+                    kwargs={"account_id":
+                                "753bcd19-7230-40ba-a975-09ac94ace0d2"}
+                ),
+
+
+            },
+            "Notification": {
+                "ListCreate Notifications": reverse(
+                    "notify:notifications",
+                    request=request,
+                    current_app="notification_management"
+                )
+            }
+        }
+        return Response(api_registry)
 
 
 class BatchTransactions(APIView):
