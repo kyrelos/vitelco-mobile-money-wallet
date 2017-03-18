76f50a2e1010d1966558082bc0985e5cb81b4a4c
kyrelos <kalosobat@gmail.com>
2017-03-18 20:59:49 +0300

    update urls

--- app_dir/wallet_transactions/urls.py
+++ app_dir/wallet_transactions/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url
-from .views import TransactionCreateList
-from app_dir.wallet_api.views import Transactions
+from .views import GetTransaction
+from app_dir.wallet_api.views import CreateTransactions
 
 urlpatterns = [
     url(r'^(?P<transaction_reference>[0-9a-zA-Z\-]+)/$',
         GetTransaction.as_view(),
         name='get_transaction_by_transaction_reference'),
-    url(r'^list_create', Transactions.as_view(),
-        name='list_create_transaction '),
+    url(r'$', CreateTransactions.as_view(),
+        name='create_transactions'),
 ]
 
