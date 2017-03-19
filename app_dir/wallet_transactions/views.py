06da8aa614bc0b3a3c52b8ae59e0f3d7ba7e0347
kyrelos <kalosobat@gmail.com>
2017-03-19 14:42:38 +0300

    add request state polling view

--- app_dir/wallet_transactions/views.py
+++ app_dir/wallet_transactions/views.py
@@ -13,6 +13,31 @@ from .serializers import TransactionSerializer
 logger = get_logger("transactions")
 
 
+def send_error_response(message="404",
+                        key="trid",
+                        value=None,
+                        status=None,
+                        ):
+    date_time = datetime.now().isoformat()
+    error_payload = {
+        "errorCategory": "businessRule",
+        "errorCode": "genericError",
+        "errorDescription": message,
+        "errorDateTime": date_time,
+        "errorParameters": [
+            {
+                "key": key,
+                "value": value
+            }
+        ]
+    }
+
+    response = Response(data=error_payload,
+                        status=status
+                        )
+    return response
+
+
 class TransactionList(generics.ListAPIView):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
@@ -45,11 +70,13 @@ class GetTransaction(APIView):
     HTTP status code: 200
     { //this is to be changed once correct
          "transaction": {
-                    "reference": transaction_reference,
-                    "type": transaction_type,
-                    "amount": transaction_amount,
-                    "date": transaction_date,
-                    "status": transaction_state
+                    "reference": "transaction_reference",
+                    "transaction_type": "transaction_type",
+                    "amount": "transaction_amount",
+                    "date": "transaction_date",
+                    "status": "transaction_state",
+                    "currency": "currency",
+                    "serverCorrelationId: "serverCorrelationId"
                 }
     }
     Error response: [404, 400, DATE header not supplied]
@@ -76,7 +103,7 @@ class GetTransaction(APIView):
                         transaction_reference=transaction_reference,
                         key="DATE"
                         )
-            return self.send_error_response(
+            return send_error_response(
                     message="DATE Header not supplied",
                     key="DATE",
                     status=status.HTTP_400_BAD_REQUEST
@@ -87,7 +114,8 @@ class GetTransaction(APIView):
             transaction_state = transaction.state
             transaction_type = transaction.transaction_type
             transaction_amount = transaction.amount
-            transaction_date = transaction.created_at
+            transaction_date = transaction.created_at.isoformat()
+            transaction_currency = transaction.currency
 
             payload = {
                 "transaction": {
@@ -95,7 +123,8 @@ class GetTransaction(APIView):
                     "transaction_type": transaction_type,
                     "amount": transaction_amount,
                     "date": transaction_date,
-                    "status": transaction_state
+                    "status": transaction_state,
+                    "currency": transaction_currency
                 }
             }
             response = Response(data=payload,
@@ -114,9 +143,9 @@ class GetTransaction(APIView):
                         key="trid"
                         )
 
-            return self.send_error_response(
+            return send_error_response(
                     message="Requested resource not available",
-                    key="trid",
+                    key="transaction_reference",
                     value=transaction_reference,
                     status=status.HTTP_404_NOT_FOUND
             )
@@ -128,34 +157,115 @@ class GetTransaction(APIView):
                         key="trid"
                         )
 
-            return self.send_error_response(
+            return send_error_response(
                     message="Malformed UUID",
-                    key="trid",
+                    key="transaction_reference",
                     value=transaction_reference,
                     status=status.HTTP_404_NOT_FOUND
             )
 
-    @staticmethod
-    def send_error_response(message="404",
-                            key="trid",
-                            value=None,
-                            status=None,
-                            ):
-        date_time = datetime.now().isoformat()
-        error_payload = {
-            "errorCategory": "businessRule",
-            "errorCode": "genericError",
-            "errorDescription": message,
-            "errorDateTime": date_time,
-            "errorParameters": [
-                {
-                    "key": key,
-                    "value": value
-                }
-            ]
-        }
 
-        response = Response(data=error_payload,
-                            status=status
-                            )
-        return response
+class GetTransactionState(APIView):
+    """
+    This API retrieves a transaction state given serverCorrelationId
+    HTTP Method: GET
+    URI: /api/v1/requeststates/{servierCorrelationId}
+    Required HTTP Headers:
+    DATE: todays date
+    AUTHORIZATION: api-key
+    CONTENT-TYPE: application/json
+    SERVERCORRELATIONID: serverCorrelationId (type: UUID)
+    Success response:
+    HTTP status code: 200
+    {
+        "reference": transaction_reference,
+        "transaction_type": transaction_type,
+        "amount": transaction_amount,
+        "date": transaction_date,
+        "status": transaction_state
+
+    }
+    Error response: [404, 400, DATE header not supplied]
+    {
+        "errorCategory": "businessRule",
+        "errorCode": "genericError",
+        "errorDescription": "string",
+        "errorDateTime": "string",
+        "errorParameters": [
+            {
+                "key": key,
+                "value": value
+            }
+        ]
+    }
+    """
+
+    def get(self, request, transaction_reference):
+        date = request.META.get("HTTP_DATE")
+        server_correlation_id = request.META.get("HTTP_X_CORRELATION_ID")
+        if not date:
+            logger.info("get_transaction_400",
+                        message="DATE Header not supplied",
+                        status=status.HTTP_400_BAD_REQUEST,
+                        transaction_reference=transaction_reference,
+                        key="DATE"
+                        )
+            return send_error_response(
+                    message="DATE Header not supplied",
+                    key="DATE",
+                    status=status.HTTP_400_BAD_REQUEST
+            )
+
+        try:
+            transaction = Transaction.objects.get(
+                    server_correlation_id=server_correlation_id)
+            transaction_state = transaction.state
+            transaction_type = transaction.transaction_type
+            transaction_amount = transaction.amount
+            transaction_date = transaction.created_at
+
+            payload = {
+                "reference": transaction_reference,
+                "transaction_type": transaction_type,
+                "amount": transaction_amount,
+                "date": transaction_date,
+                "status": transaction_state,
+                "serverCorrelationId": server_correlation_id,
+
+            }
+            response = Response(data=payload,
+                                status=status.HTTP_200_OK
+                                )
+            logger.info("get_transaction_state_200",
+                        status=status.HTTP_200_OK,
+                        serverCorrelationId=server_correlation_id
+                        )
+            return response
+
+        except ObjectDoesNotExist:
+            logger.info("get_transaction_state_404",
+                        status=status.HTTP_404_NOT_FOUND,
+                        trid=transaction_reference,
+                        key="trid"
+                        )
+
+            return send_error_response(
+                    message="Requested resource not available",
+                    key="serverCorrelationId",
+                    value=server_correlation_id,
+                    status=status.HTTP_404_NOT_FOUND
+            )
+
+        except ValueError:
+            logger.info("get_transaction_state_malformed_uuid",
+                        status=status.HTTP_400_BAD_REQUEST,
+                        trid=transaction_reference,
+                        key="serverCorrelationId"
+                        )
+
+            return send_error_response(
+                    message="Malformed UUID string",
+                    key="serverCorrelationId",
+                    value=server_correlation_id,
+                    status=status.HTTP_400_BAD_REQUEST
+            )
