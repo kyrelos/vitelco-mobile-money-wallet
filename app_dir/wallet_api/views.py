c0f4d12ad08e80b9e8daf8de4238356f5ede673b
kyrelos <kalosobat@gmail.com>
2017-03-20 21:11:30 +0300

    fix pep8

--- app_dir/wallet_api/views.py
+++ app_dir/wallet_api/views.py
@@ -1,11 +1,8 @@
 import uuid
 from datetime import datetime
-from time import gmtime, strftime
-import requests
+
 from django.conf import settings
 from django.db import IntegrityError
-from django.conf import settings
-from django.core.exceptions import ObjectDoesNotExist
 from django.forms.models import model_to_dict
 from django.http import Http404
 from rest_framework import status
@@ -16,9 +13,9 @@ from structlog import get_logger
 
 from app_dir.customer_wallet_management.models import CustomerWallet
 from app_dir.wallet_transactions.models import BatchTransaction, \
-    Transaction, BatchTransactionLookup
+    BatchTransactionLookup, Transaction
 from app_dir.wallet_transactions.serializers import \
-    BatchTransactionSerializer, BatchTransactionLookUpSerializer
+    BatchTransactionSerializer
 
 logger = get_logger('transactions')
 
@@ -28,85 +25,85 @@ class APIRootView(APIView):
         api_registry = {
             "Transactions": {
                 "Get transaction by transaction_reference": reverse(
-                    "get_transaction_by_transaction_reference",
-                    request=request,
-                    current_app="wallet_transactions",
-                    kwargs={
-                        "transaction_reference":
-                            "753bcd19-7230-40ba-a975-09ac94ace0d2"
-                    }
+                        "get_transaction_by_transaction_reference",
+                        request=request,
+                        current_app="wallet_transactions",
+                        kwargs={
+                            "transaction_reference":
+                                "753bcd19-7230-40ba-a975-09ac94ace0d2"
+                        }
 
                 ),
                 "Create transactions": reverse(
-                    "create_transactions",
-                    request=request
+                        "create_transactions",
+                        request=request
                 ),
                 "Get transaction state": reverse(
-                    "get_transaction_state",
-                    request=request,
-                    kwargs={
-                        "server_correlation_id":
-                            "753bcd19-7230-40ba-a975-09ac94ace0d2"
-                    }
+                        "get_transaction_state",
+                        request=request,
+                        kwargs={
+                            "server_correlation_id":
+                                "753bcd19-7230-40ba-a975-09ac94ace0d2"
+                        }
                 )
 
             },
             "BatchTransactions": {
                 "Create batch transactions": reverse(
-                    "batchtransactions", request=request
+                        "batchtransactions", request=request
                 ),
                 "Get batch transaction": reverse(
-                    "get_batch_transaction", request=request
+                        "get_batch_transaction", request=request
                 )
             },
             "Account": {
                 "Get Account status by msisdn": reverse(
-                    "account:msisdn",
-                    request=request,
-                    current_app="customer_wallet_management",
-                    kwargs={"msisdn": "+254711111111"}
+                        "account:msisdn",
+                        request=request,
+                        current_app="customer_wallet_management",
+                        kwargs={"msisdn": "+254711111111"}
                 ),
                 "Get Account status by accountId": reverse(
-                    "account:get_account_status_by_account_id",
-                    request=request,
-                    current_app="customer_wallet_management",
-                    kwargs={
-                        "wallet_id": "753bcd19-7230-40ba-a975-09ac94ace0d2"
-                    }
+                        "account:get_account_status_by_account_id",
+                        request=request,
+                        current_app="customer_wallet_management",
+                        kwargs={
+                            "wallet_id": "753bcd19-7230-40ba-a975-09ac94ace0d2"
+                        }
                 ),
                 "Get Account name by msisdn": reverse(
-                    "account:get_account_name_by_msisdn",
-                    request=request,
-                    current_app="customer_wallet_management",
-                    kwargs={"msisdn": "254711111111"}
+                        "account:get_account_name_by_msisdn",
+                        request=request,
+                        current_app="customer_wallet_management",
+                        kwargs={"msisdn": "254711111111"}
                 ),
                 "Get Account name by accountId": reverse(
-                    "account:get_account_name_by_account_id",
-                    request=request,
-                    current_app="customer_wallet_management",
-                    kwargs={"account_id":
-                                "753bcd19-7230-40ba-a975-09ac94ace0d2"}
+                        "account:get_account_name_by_account_id",
+                        request=request,
+                        current_app="customer_wallet_management",
+                        kwargs={"account_id":
+                                    "753bcd19-7230-40ba-a975-09ac94ace0d2"}
                 ),
                 "Get Account balance by msisdn": reverse(
-                    "account:get_account_balance_by_msisdn",
-                    request=request,
-                    current_app="customer_wallet_management",
-                    kwargs={"msisdn": "254711111111"}
+                        "account:get_account_balance_by_msisdn",
+                        request=request,
+                        current_app="customer_wallet_management",
+                        kwargs={"msisdn": "254711111111"}
                 ),
                 "Get Account balance by accountId": reverse(
-                    "account:get_account_balance_by_account_id",
-                    request=request,
-                    current_app="customer_wallet_management",
-                    kwargs={"account_id":
-                                "753bcd19-7230-40ba-a975-09ac94ace0d2"}
+                        "account:get_account_balance_by_account_id",
+                        request=request,
+                        current_app="customer_wallet_management",
+                        kwargs={"account_id":
+                                    "753bcd19-7230-40ba-a975-09ac94ace0d2"}
                 ),
 
             },
             "Notification": {
                 "ListCreate Notifications": reverse(
-                    "notify:notifications",
-                    request=request,
-                    current_app="notification_management"
+                        "notify:notifications",
+                        request=request,
+                        current_app="notification_management"
                 )
             }
         }
@@ -149,58 +146,60 @@ def send_error_response(message="404",
                         )
     return response
 
+
 class GetBatchTransaction(APIView):
     """
     This API allows retrieving a bulk transaction given an ID
     """
+
     def get(self, request, batch_trid=None):
         if not batch_trid:
             logger.info(
-                "get_transaction_invalid_uuid",
-                status=status.HTTP_400_BAD_REQUEST,
-                batch_trid=batch_trid,
-                key="batch_trid"
+                    "get_transaction_invalid_uuid",
+                    status=status.HTTP_400_BAD_REQUEST,
+                    batch_trid=batch_trid,
+                    key="batch_trid"
             )
 
             return send_error_response(
-                message="Invalid UUID",
-                key="batch_transaction_reference",
-                status=status.HTTP_400_BAD_REQUEST
+                    message="Invalid UUID",
+                    key="batch_transaction_reference",
+                    status=status.HTTP_400_BAD_REQUEST
             )
 
         date = request.META.get("HTTP_DATE")
         if not date and not settings.DEBUG:
             logger.info(
-                "get_bulk_transaction_400",
-                message="DATE Header not supplied",
-                status=status.HTTP_400_BAD_REQUEST,
-                key="DATE"
+                    "get_bulk_transaction_400",
+                    message="DATE Header not supplied",
+                    status=status.HTTP_400_BAD_REQUEST,
+                    key="DATE"
             )
 
             return send_error_response(
-                message="DATE Header not supplied",
-                key="DATE",
-                status=status.HTTP_400_BAD_REQUEST
+                    message="DATE Header not supplied",
+                    key="DATE",
+                    status=status.HTTP_400_BAD_REQUEST
             )
 
         try:
             batch_transactions = BatchTransactionLookup.objects.filter(
-                batch_transaction__batch_trid=batch_trid
+                    batch_transaction__batch_trid=batch_trid
             )
 
             if not batch_transactions:
                 logger.info(
-                    "get_batch_transaction_404",
-                    status=status.HTTP_404_NOT_FOUND,
-                    batch_trid=batch_trid,
-                    key="batch_trid"
+                        "get_batch_transaction_404",
+                        status=status.HTTP_404_NOT_FOUND,
+                        batch_trid=batch_trid,
+                        key="batch_trid"
                 )
 
                 return send_error_response(
-                    message="Requested resource not available",
-                    key="batch_transaction_reference",
-                    value=batch_trid,
-                    status=status.HTTP_404_NOT_FOUND
+                        message="Requested resource not available",
+                        key="batch_transaction_reference",
+                        value=batch_trid,
+                        status=status.HTTP_404_NOT_FOUND
                 )
 
             payload = {
@@ -210,31 +209,32 @@ class GetBatchTransaction(APIView):
 
             for transaction in batch_transactions:
                 payload['transactions'].append(
-                    model_to_dict(transaction.transaction))
+                        model_to_dict(transaction.transaction))
 
             response = Response(
-                data=payload, status=status.HTTP_200_OK
+                    data=payload, status=status.HTTP_200_OK
             )
             logger.info(
-                "get_batch_transaction_200",
-                status=status.HTTP_200_OK,
-                batch_trid=batch_trid
+                    "get_batch_transaction_200",
+                    status=status.HTTP_200_OK,
+                    batch_trid=batch_trid
             )
             return response
         except ValueError:
             logger.info(
-                "get_transaction_malformed_uuid",
-                status=status.HTTP_404_NOT_FOUND,
-                batch_trid=batch_trid,
-                key="batch_trid"
+                    "get_transaction_malformed_uuid",
+                    status=status.HTTP_404_NOT_FOUND,
+                    batch_trid=batch_trid,
+                    key="batch_trid"
             )
 
             return send_error_response(
-                message="Malformed UUID",
-                key="batch_transaction_reference",
-                value=batch_trid,
-                status=status.HTTP_404_NOT_FOUND
+                    message="Malformed UUID",
+                    key="batch_transaction_reference",
+                    value=batch_trid,
+                    status=status.HTTP_404_NOT_FOUND
             )
+
 
 class BatchTransactions(APIView):
     """
@@ -324,28 +324,28 @@ class BatchTransactions(APIView):
                 destination_msisdn = data["creditParty"][0]["value"]
 
                 destination = CustomerWallet.objects.get(
-                    msisdn=destination_msisdn)
+                        msisdn=destination_msisdn)
                 amount = data["amount"]
                 transaction_type = data["type"]
                 create_transaction_data = dict(
-                    source=source,
-                    server_correlation_id=server_correlation_id,
-                    destination=destination,
-                    amount=amount,
-                    transaction_type=transaction_type
+                        source=source,
+                        server_correlation_id=server_correlation_id,
+                        destination=destination,
+                        amount=amount,
+                        transaction_type=transaction_type
                 )
 
                 trx = Transaction.objects.create(
-                    **create_transaction_data)
+                        **create_transaction_data)
 
                 batch_lookup = BatchTransactionLookup.objects.create(
-                    batch_transaction=batch_trx,
-                    transaction=trx)
+                        batch_transaction=batch_trx,
+                        transaction=trx)
 
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return_errors = dict(
-                batch_transaction_errors=serializer.error_messages, )
+                    batch_transaction_errors=serializer.error_messages, )
             return Response(return_errors,
                             status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
@@ -404,9 +404,9 @@ class CreateTransactions(APIView):
                         key=e.message
                         )
             return send_error_response(
-                message="Required Headers not supplied",
-                key=e.message,
-                status=status.HTTP_400_BAD_REQUEST
+                    message="Required Headers not supplied",
+                    key=e.message,
+                    status=status.HTTP_400_BAD_REQUEST
             )
         try:
             data = request.data
@@ -418,12 +418,12 @@ class CreateTransactions(APIView):
             amount = float(data["amount"])
             transaction_type = data["type"]
             create_transaction_data = dict(
-                trid=trid,
-                source=source,
-                server_correlation_id=server_correlation_id,
-                destination=destination,
-                amount=amount,
-                transaction_type=transaction_type
+                    trid=trid,
+                    source=source,
+                    server_correlation_id=server_correlation_id,
+                    destination=destination,
+                    amount=amount,
+                    transaction_type=transaction_type
             )
             source_balance = source.get_available_balance()
             if transaction_type == 'transfer':
@@ -435,10 +435,10 @@ class CreateTransactions(APIView):
                             )
                 if source_balance < amount:
                     insufficient_funds_response = send_error_response(
-                        message="You have insufficient funds",
-                        key="Balance",
-                        value=source_balance,
-                        status=status.HTTP_402_PAYMENT_REQUIRED
+                            message="You have insufficient funds",
+                            key="Balance",
+                            value=source_balance,
+                            status=status.HTTP_402_PAYMENT_REQUIRED
                     )
                     return insufficient_funds_response
 
@@ -449,10 +449,10 @@ class CreateTransactions(APIView):
             status_code = status.HTTP_400_BAD_REQUEST
 
             error_response = send_error_response(
-                message=error_message,
-                key=key,
-                value=value,
-                status=status_code
+                    message=error_message,
+                    key=key,
+                    value=value,
+                    status=status_code
             )
             logger.info("create_transaction_400",
                         status=status.HTTP_400_BAD_REQUEST,
@@ -464,7 +464,7 @@ class CreateTransactions(APIView):
             count = 0
             while count < 10:
                 transaction, exception = self.create_transaction(
-                    create_transaction_data)
+                        create_transaction_data)
                 if transaction:
                     response_payload = {
                         "objectReference": trid,
@@ -499,10 +499,10 @@ class CreateTransactions(APIView):
                     count += 1
 
             return send_error_response(
-                message=error_message,
-                key=error_key,
-                status=status.HTTP_409_CONFLICT,
-                value="Duplicate UUID"
+                    message=error_message,
+                    key=error_key,
+                    status=status.HTTP_409_CONFLICT,
+                    value="Duplicate UUID"
 
             )
 
