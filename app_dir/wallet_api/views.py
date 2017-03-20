e0ec4e7c53e6d95c6cc24a137d29c7f5876abb8b
kyrelos <kalosobat@gmail.com>
2017-03-20 21:35:02 +0300

    update root api-view

--- app_dir/wallet_api/views.py
+++ app_dir/wallet_api/views.py
@@ -25,13 +25,13 @@ class APIRootView(APIView):
         api_registry = {
             "Transactions": {
                 "Get transaction by transaction_reference": reverse(
-                        "get_transaction_by_transaction_reference",
-                        request=request,
-                        current_app="wallet_transactions",
-                        kwargs={
-                            "transaction_reference":
-                                "753bcd19-7230-40ba-a975-09ac94ace0d2"
-                        }
+                    "get_transaction_by_transaction_reference",
+                    request=request,
+                    current_app="wallet_transactions",
+                    kwargs={
+                        "transaction_reference":
+                            "753bcd19-7230-40ba-a975-09ac94ace0d2"
+                    }
 
                 ),
                 "Create transactions": reverse(
@@ -39,12 +39,12 @@ class APIRootView(APIView):
                         request=request
                 ),
                 "Get transaction state": reverse(
-                        "get_transaction_state",
-                        request=request,
-                        kwargs={
-                            "server_correlation_id":
-                                "753bcd19-7230-40ba-a975-09ac94ace0d2"
-                        }
+                    "get_transaction_state",
+                    request=request,
+                    kwargs={
+                        "server_correlation_id":
+                            "753bcd19-7230-40ba-a975-09ac94ace0d2"
+                    }
                 )
 
             },
@@ -53,57 +53,63 @@ class APIRootView(APIView):
                         "batchtransactions", request=request
                 ),
                 "Get batch transaction": reverse(
-                        "get_batch_transaction", request=request
+                    "get_batch_transaction",
+                    request=request,
+                    kwargs={
+                        "batch_trid":
+                            "753bcd19-7230-40ba-a975-09ac94ace0d2"
+                    }
+
                 )
             },
             "Account": {
                 "Get Account status by msisdn": reverse(
-                        "account:msisdn",
-                        request=request,
-                        current_app="customer_wallet_management",
-                        kwargs={"msisdn": "+254711111111"}
+                    "account:msisdn",
+                    request=request,
+                    current_app="customer_wallet_management",
+                    kwargs={"msisdn": "+254711111111"}
                 ),
                 "Get Account status by accountId": reverse(
-                        "account:get_account_status_by_account_id",
-                        request=request,
-                        current_app="customer_wallet_management",
-                        kwargs={
-                            "wallet_id": "753bcd19-7230-40ba-a975-09ac94ace0d2"
-                        }
+                    "account:get_account_status_by_account_id",
+                    request=request,
+                    current_app="customer_wallet_management",
+                    kwargs={
+                        "wallet_id": "753bcd19-7230-40ba-a975-09ac94ace0d2"
+                    }
                 ),
                 "Get Account name by msisdn": reverse(
-                        "account:get_account_name_by_msisdn",
-                        request=request,
-                        current_app="customer_wallet_management",
-                        kwargs={"msisdn": "254711111111"}
+                    "account:get_account_name_by_msisdn",
+                    request=request,
+                    current_app="customer_wallet_management",
+                    kwargs={"msisdn": "254711111111"}
                 ),
                 "Get Account name by accountId": reverse(
-                        "account:get_account_name_by_account_id",
-                        request=request,
-                        current_app="customer_wallet_management",
-                        kwargs={"account_id":
-                                    "753bcd19-7230-40ba-a975-09ac94ace0d2"}
+                    "account:get_account_name_by_account_id",
+                    request=request,
+                    current_app="customer_wallet_management",
+                    kwargs={"account_id":
+                                "753bcd19-7230-40ba-a975-09ac94ace0d2"}
                 ),
                 "Get Account balance by msisdn": reverse(
-                        "account:get_account_balance_by_msisdn",
-                        request=request,
-                        current_app="customer_wallet_management",
-                        kwargs={"msisdn": "254711111111"}
+                    "account:get_account_balance_by_msisdn",
+                    request=request,
+                    current_app="customer_wallet_management",
+                    kwargs={"msisdn": "254711111111"}
                 ),
                 "Get Account balance by accountId": reverse(
-                        "account:get_account_balance_by_account_id",
-                        request=request,
-                        current_app="customer_wallet_management",
-                        kwargs={"account_id":
-                                    "753bcd19-7230-40ba-a975-09ac94ace0d2"}
+                    "account:get_account_balance_by_account_id",
+                    request=request,
+                    current_app="customer_wallet_management",
+                    kwargs={"account_id":
+                                "753bcd19-7230-40ba-a975-09ac94ace0d2"}
                 ),
 
             },
             "Notification": {
                 "ListCreate Notifications": reverse(
-                        "notify:notifications",
-                        request=request,
-                        current_app="notification_management"
+                    "notify:notifications",
+                    request=request,
+                    current_app="notification_management"
                 )
             }
         }
