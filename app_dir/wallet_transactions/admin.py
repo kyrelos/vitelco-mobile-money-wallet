ff3c89703bdf59364a770cbabd59b5ec5c1757dc
kyrelos <kalosobat@gmail.com>
2017-03-20 21:08:31 +0300

    add wallet topup transaction objects to admin interface

--- app_dir/wallet_transactions/admin.py
+++ app_dir/wallet_transactions/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Transaction, BatchTransaction, BatchTransactionLookup
+from .models import Transaction, BatchTransaction, BatchTransactionLookup, \
+    WalletTopupTransaction
 
 admin.site.register(Transaction)
 admin.site.register(BatchTransaction)
 admin.site.register(BatchTransactionLookup)
+admin.site.register(WalletTopupTransaction)
+
