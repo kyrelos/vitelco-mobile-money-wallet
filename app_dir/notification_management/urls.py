72ec48cc05d0c733408d9874c312a65d90bcbb54
kyrelos <kalosobat@gmail.com>
2017-03-21 20:16:49 +0300

    add update notification urls

--- app_dir/notification_management/urls.py
+++ app_dir/notification_management/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
-from .views import NotificationList
+from .views import NotificationList, UpdateNotification
 
 
 urlpatterns = [
     url(r'^list_create', NotificationList.as_view(), name='notifications'),
+    url(r'^updatenotification', UpdateNotification.as_view(),
+        name='update_notifications'),
 
 
 ]
