903712f50e83ed0825e62aff672a6ccd79d7a4f7
kyrelos <kalosobat@gmail.com>
2017-03-21 15:24:32 +0300

    handle update notification requests

--- app_dir/notification_management/views.py
+++ app_dir/notification_management/views.py
@@ -1,4 +1,9 @@
 from rest_framework import generics
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from app_dir.notification_management.tasks import send_normal_notification
 from .models import Notification
 from .serializers import NotificationSerializer
 from rest_framework.permissions import IsAdminUser
@@ -30,3 +35,29 @@ class NotificationList(generics.ListCreateAPIView):
     queryset = Notification.objects.all()
     serializer_class = NotificationSerializer
     permission_classes = (IsAdminUser, )
+
+
+class UpdateNotification(APIView):
+    def post(self, request):
+        request_data = request.data
+        request_type = request_data.get("request_type")
+        if request_type == "token_refresh":
+            msisdn = request_data["msisdn"]
+            token = request_data["token"]
+            response = Response(status=status.HTTP_200_OK)
+            return response
+
+        else:
+            notification_id = request_data['notification_id']
+
+            transaction_id = request_data['transaction_id']
+            pin = request_data['pin']
+            push_status = request_data['status']
+            if str(pin) == "1234":
+                send_normal_notification.delay(notification_id, transaction_id)
+
+            response = Response(status=status.HTTP_200_OK)
+            return response
+
+
+
