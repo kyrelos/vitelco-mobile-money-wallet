41c9e426184cbeac24550081acfac6e6efffe8c8
kyrelos <kalosobat@gmail.com>
2017-03-21 15:23:18 +0300

    add device map to admin interface

--- app_dir/notification_management/admin.py
+++ app_dir/notification_management/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Notification
+from .models import Notification, NotificationDeviceMap
 
 admin.site.register(Notification)
+admin.site.register(NotificationDeviceMap)
