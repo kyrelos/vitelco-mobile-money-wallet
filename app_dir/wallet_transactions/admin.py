510d106b038643770b058861bd261ad29d075ad3
kyrelos <kalosobat@gmail.com>
2017-03-24 15:32:50 +0300

    add modeladmin

--- app_dir/wallet_transactions/admin.py
+++ app_dir/wallet_transactions/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 from .models import Transaction, BatchTransaction, BatchTransactionLookup, \
     WalletTopupTransaction, DebitMandate
 
-admin.site.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ["trid", "created_at", "modified_at", "amount",
+                    "transaction_type", "source",
+                    "destination"]
+    search_fields = ["trid"]
+
+admin.site.register(Transaction, TransactionAdmin)
 admin.site.register(BatchTransaction)
 admin.site.register(BatchTransactionLookup)
 admin.site.register(WalletTopupTransaction)
